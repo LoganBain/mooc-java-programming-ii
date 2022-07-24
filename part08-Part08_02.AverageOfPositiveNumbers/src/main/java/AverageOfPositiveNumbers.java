90d0fb28e405fca145752f0d83dfa5f0474acd44
LoganBain <loganbain23@gmail.com>
2022-07-24 01:41:41 +0100

    Completed Part 8 Exercise 2

--- part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ part08-Part08_02.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,28 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        
+        int sumOfNumbers = 0;
+        int amtOfNumbers = 0;
+        
+        while (true) {
+            int input = Integer.valueOf(scanner.nextLine());
+            
+            if (input == 0) {
+                break;
+            }
 
+            if (input > 0) {
+                sumOfNumbers += input;
+                amtOfNumbers++;
+                continue;
+            }
+        } 
+        if (sumOfNumbers < 1) {
+            System.out.println("Cannot calculate average");
+        } else {
+            double average = 1.0 * sumOfNumbers / amtOfNumbers;
+            System.out.println(average);
+        }
     }
 }
