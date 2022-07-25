1e39b6a1363ee6d796fb362a361c08dea4c8f61b
LoganBain <loganbain23@gmail.com>
2022-07-25 19:27:45 +0100

    Completed Part 8 Exercise 9

--- part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
+++ part08-Part08_09.PrintMeAnotherHashmap/src/main/java/Program.java
@@ -6,5 +6,19 @@ public class Program {
     public static void main(String[] args) {
         // Test your program here!
     }
+    
+    public static void printValues(HashMap<String, Book> hashmap) {
+        for (Book book : hashmap.values()) {
+            System.out.println(book);
+        }
+    }
+    
+    public static void printValueIfNameContains(HashMap<String, Book> hashmap, String text) {
+        for (Book book : hashmap.values()) {
+            if (book.getName().contains(text)) {
+                System.out.println(book);
+            }
+        }
+    }
 
 }
