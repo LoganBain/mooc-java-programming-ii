bef0b626616008c705d9160822b61fbb55e9d17c
LoganBain <loganbain23@gmail.com>
2022-07-26 23:55:40 +0100

    Completed Part 8 Exercise 11

--- part08-Part08_11.SameDate/src/main/java/SimpleDate.java
+++ part08-Part08_11.SameDate/src/main/java/SimpleDate.java
@@ -50,5 +50,28 @@ public class SimpleDate {
 
         return this.year - other.year - yearRemoved;
     }
-
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final SimpleDate other = (SimpleDate) obj;
+        if (this.day != other.day) {
+            return false;
+        }
+        if (this.month != other.month) {
+            return false;
+        }
+        if (this.year != other.year) {
+            return false;
+        }
+        return true;
+    }    
 }
