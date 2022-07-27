0064e5efd6510b047c606a935658f0d9b613f880
LoganBain <loganbain23@gmail.com>
2022-07-28 00:09:22 +0100

    Completed Part 9 Exercise 1

--- part09-Part09_01.ABC/src/main/java/Main.java
+++ part09-Part09_01.ABC/src/main/java/Main.java
@@ -3,6 +3,10 @@ public class Main {
 
     public static void main(String[] args) {
         // write your test code here
+        C c = new C();
         
+        c.a();
+        c.b();
+        c.c();
     }
 }
