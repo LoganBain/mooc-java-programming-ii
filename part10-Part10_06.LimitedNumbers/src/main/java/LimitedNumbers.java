324d483c50e0a96d081cf32665e217141af79a93
LoganBain <loganbain23@gmail.com>
2022-08-30 07:32:06 +0100

    Completed Part 10 Exercise 6

--- part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
+++ part10-Part10_06.LimitedNumbers/src/main/java/LimitedNumbers.java
@@ -6,6 +6,18 @@ public class LimitedNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Integer> numbers = new ArrayList<>();
 
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (Integer.valueOf(input) < 0) {
+                break;
+            }
+            
+            numbers.add(Integer.valueOf(input));
+        }
+        
+        numbers.stream().filter(n -> n > 0 && n < 6).forEach(n -> System.out.println(n));
     }
 }
