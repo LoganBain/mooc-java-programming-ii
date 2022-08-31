af186467e85dbd871591bd5957f9ca2c069ce386
LoganBain <loganbain23@gmail.com>
2022-08-31 12:14:58 +0100

    Completed Part 10 Exercise 13

--- part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
+++ part10-Part10_13.LiteracyComparison/src/main/java/LiteracyComparison.java
@@ -7,6 +7,15 @@ import java.util.ArrayList;
 public class LiteracyComparison {
     
     public static void main(String[] args) {
-
+        
+        try {
+            Files.lines(Paths.get("literacy.csv"))
+                    .map(s -> s.split(","))
+                    .sorted((x, y) -> x[5].compareTo(y[5]))
+                    .forEach(v -> System.out.println(v[3] + " (" + Integer.parseInt(v[4]) + "), " + v[2].split(" ")[1].trim() + ", " + Double.parseDouble(v[5])));
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
