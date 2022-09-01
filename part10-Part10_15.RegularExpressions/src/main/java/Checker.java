9fd6fa8cac07434ba9421556511733a19292fc03
LoganBain <loganbain23@gmail.com>
2022-09-01 14:00:15 +0100

    Completed Part 10 Exercise 15

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -2,14 +2,21 @@
 
 public class Checker {
     public boolean isDayOfWeek(String string) {
-        if (string.matches("mon|tue|wed|thu|fri")) {
+        if (string.matches("mon|tue|wed|thu|fri|sat|sun")) {
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean allVowels(String string) {
+        if (string.matches("^[aeiou]*")) {
             return true;
         }
         return false;
     }
     
     public boolean timeOfDay(String string) {
-        if (string.matches("[0-2][0-3]:[0-5][0-9]:[0-5][0-9]")) {
+        if (string.matches("([01][0123456789]|2[0123]):[012345][0123456789]:[012345][0123456789]")) {
             return true;
         }
         return false;
