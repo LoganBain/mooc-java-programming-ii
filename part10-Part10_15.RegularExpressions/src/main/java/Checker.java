4fcdf11d21df76fdc86253565e5edcf5f7a58641
LoganBain <loganbain23@gmail.com>
2022-09-01 11:54:51 +0100

    Completed Part 10 Exercise 15

--- part10-Part10_15.RegularExpressions/src/main/java/Checker.java
+++ part10-Part10_15.RegularExpressions/src/main/java/Checker.java
@@ -1,5 +1,17 @@
 
 
 public class Checker {
-
+    public boolean isDayOfWeek(String string) {
+        if (string.matches("mon|tue|wed|thu|fri")) {
+            return true;
+        }
+        return false;
+    }
+    
+    public boolean timeOfDay(String string) {
+        if (string.matches("[0-2][0-3]:[0-5][0-9]:[0-5][0-9]")) {
+            return true;
+        }
+        return false;
+    }
 }
--- part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
+++ part10-Part10_15.RegularExpressions/src/main/java/MainProgram.java
@@ -1,9 +1,27 @@
 
+import java.util.Scanner;
 
 public class MainProgram {
 
     public static void main(String[] args) {
         // you can create test code here. Call the methods that you'll implement
         // during the course of this exercise
+        Scanner scanner = new Scanner(System.in);
+        Checker check = new Checker();
+        
+        while (true) {
+            System.out.println("Enter a string:");
+            if (check.isDayOfWeek(scanner.nextLine())) {
+                System.out.println("The form is correct.");
+            } else {
+                System.out.println("The form is incorrect.");
+            }
+            
+            if (check.timeOfDay(scanner.nextLine())) {
+                System.out.println("The form is correct.");
+            } else {
+                System.out.println("The form is incorrect.");
+            }
+        }
     }
 }
