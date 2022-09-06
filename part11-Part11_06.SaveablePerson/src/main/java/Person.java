948575f32c335f686763961cb662befce54f254a
LoganBain <loganbain23@gmail.com>
2022-09-06 22:12:44 +0100

    Completed Part 11 Exercise 6

---
+++ part11-Part11_06.SaveablePerson/src/main/java/Person.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author logan
+ */
+public class Person implements Saveable {
+    private String name;
+    private String address;
+    
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void delete() {
+
+    }
+
+    @Override
+    public void load(String address) {
+
+    }
+    
+}
---
+++ part11-Part11_06.SaveablePerson/src/main/java/Saveable.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author logan
+ */
+public interface Saveable {
+    public void save();
+    
+    public void delete();
+    
+    public void load(String address);
+}
