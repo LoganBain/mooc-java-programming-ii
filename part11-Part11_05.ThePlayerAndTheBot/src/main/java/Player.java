77a2648260b7baf371c602652d3f3eb1c4b12fb5
LoganBain <loganbain23@gmail.com>
2022-09-06 21:56:00 +0100

    Completed Part 11 Exercise 5

---
+++ part11-Part11_05.ThePlayerAndTheBot/src/main/java/Player.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author logan
+ */
+public class Player {
+    private String name;
+    
+    public void play() {
+        
+    }
+    
+    public void printName() {
+        
+    }
+}
