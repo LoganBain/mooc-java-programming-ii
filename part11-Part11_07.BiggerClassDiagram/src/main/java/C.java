15aac899734d314bf90c5a63d6c37ae4fe215b87
LoganBain <loganbain23@gmail.com>
2022-09-06 22:20:58 +0100

    Completed Part 11 Exercise 7

---
+++ part11-Part11_07.BiggerClassDiagram/src/main/java/C.java
@@ -0,0 +1,16 @@
+
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author logan
+ */
+public class C extends B implements IC {
+    private List<E> e;
+}
---
+++ part11-Part11_07.BiggerClassDiagram/src/main/java/E.java
@@ -0,0 +1,16 @@
+
+import java.util.List;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author logan
+ */
+public class E {
+    private List<C> c;
+}
