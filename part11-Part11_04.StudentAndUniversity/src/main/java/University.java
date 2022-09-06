a0a06683425f1de72bd2619cddf833e57a0fa4dd
LoganBain <loganbain23@gmail.com>
2022-09-06 21:49:49 +0100

    Completed Part 11 Exercise 4

---
+++ part11-Part11_04.StudentAndUniversity/src/main/java/University.java
@@ -0,0 +1,19 @@
+
+import java.util.List;
+
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author logan
+ */
+public class University {
+    private String name;
+    private List<Student> students;
+}
