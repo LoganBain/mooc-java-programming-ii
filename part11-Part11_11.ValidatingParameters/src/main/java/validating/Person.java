6c5bc8f01a86e23486dedd3ebf9736ad54c9c616
LoganBain <loganbain23@gmail.com>
2022-09-11 08:02:22 +0100

    Completed Part 11 Exercise 11

--- part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
+++ part11-Part11_11.ValidatingParameters/src/main/java/validating/Person.java
@@ -6,7 +6,14 @@ public class Person {
     private int age;
 
     public Person(String name, int age) {
-
+        if (name == null || name.isEmpty() || name.length() > 40) {
+            throw new IllegalArgumentException("Invalid name.");
+        }
+        
+        if (age < 0 || age > 120) {
+            throw new IllegalArgumentException("Age must be between 0 and 120 inclusive.");
+        }
+        
         this.name = name;
         this.age = age;
     }
