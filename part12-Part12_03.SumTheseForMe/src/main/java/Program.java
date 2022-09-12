5240ea02138c7790a3040ccd30bf6992bf7b788c
LoganBain <loganbain23@gmail.com>
2022-09-12 12:57:07 +0100

    Completed Part 12 Exercise 3

--- part12-Part12_03.SumTheseForMe/src/main/java/Program.java
+++ part12-Part12_03.SumTheseForMe/src/main/java/Program.java
@@ -7,6 +7,35 @@ public class Program {
         Scanner scan = new Scanner(System.in);
         // test your method here
 
+        int[] numbers = {3, -1, 8, 4};
+
+        System.out.println(sum(numbers, 0, 0, 0, 0));
+        System.out.println(sum(numbers, 0, 0, 0, 10));
+        System.out.println(sum(numbers, 0, 1, 0, 10));
+        System.out.println(sum(numbers, 0, 1, -10, 10));
+        System.out.println(sum(numbers, -1, 999, -10, 10));
+
+    }
+
+    public static int sum(int[] array, int fromWhere, int toWhere, int smallest, int largest) {
+
+        int sum = 0;
+
+        if (fromWhere < 0) {
+            fromWhere = 0;
+        }
+
+        if (toWhere > array.length) {
+            toWhere = array.length;
+        }
+
+        for (int i = fromWhere; i < toWhere; i++) {
+            if (array[i] <= largest && array[i] >= smallest) {
+                sum += array[i];
+            }
+        }
+
+        return sum;
     }
 
 }
