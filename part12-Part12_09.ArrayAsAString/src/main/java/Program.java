1ea17f18abfd57c8a8c910039c1c3fae08588790
LoganBain <loganbain23@gmail.com>
2022-09-22 22:46:04 +0100

    Completed Part 12 Exercise 9

--- part12-Part12_09.ArrayAsAString/src/main/java/Program.java
+++ part12-Part12_09.ArrayAsAString/src/main/java/Program.java
@@ -3,6 +3,26 @@ public class Program {
 
     public static void main(String[] args) {
         // Test your method here
+        int rows = 2;
+        int columns = 3;
+        int[][] matrix = new int[rows][columns];
+        matrix[0][1] = 5;
+        matrix[1][0] = 3;
+        matrix[1][2] = 7;
+        System.out.println(arrayAsString(matrix));
+    }
+
+    public static String arrayAsString(int[][] array) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int row = 0; row < array.length; row++) {
+            for (int column = 0; column < array[row].length; column++) {
+                int value = array[row][column];
+                sb.append(value);
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
     }
 
 }
