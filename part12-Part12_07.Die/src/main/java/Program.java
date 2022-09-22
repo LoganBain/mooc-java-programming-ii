a4d000f05d245cc85a757d2c687bb33e609de9fd
LoganBain <loganbain23@gmail.com>
2022-09-22 19:35:40 +0100

    Completed Part 12 Exercise 7

--- part12-Part12_07.Die/src/main/java/Program.java
+++ part12-Part12_07.Die/src/main/java/Program.java
@@ -3,7 +3,7 @@ public class Program {
 
     public static void main(String[] args) {
         // main program
-        Die die = new Die(6);
+        Die die = new Die(16);
 
         int i = 0;
         while (i < 10) {
