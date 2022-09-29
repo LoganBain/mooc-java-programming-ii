c844dd77d63cc67f882527e232e70e56df82f969
LoganBain <loganbain23@gmail.com>
2022-09-29 19:25:32 +0100

    Completed Part 13 Exercise 2

--- part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
+++ part13-Part13_02.ButtonAndLabel/src/main/java/buttonandlabel/ButtonAndLabelApplication.java
@@ -1,11 +1,32 @@
 package buttonandlabel;
 
+import javafx.scene.control.Button;
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.control.Label;
+import javafx.scene.layout.FlowPane;
+import javafx.scene.Scene;
 
-public class ButtonAndLabelApplication {
 
-
+public class ButtonAndLabelApplication extends Application {
+    
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ButtonAndLabelApplication.class);
+    }
+
+    @Override
+    public void start(Stage window) {
+        Button button = new Button("Hello Button");
+        Label label = new Label("Hello Label");
+        
+        FlowPane componentGroup = new FlowPane();
+        componentGroup.getChildren().add(button);
+        componentGroup.getChildren().add(label);
+        
+        Scene view = new Scene(componentGroup);
+        
+        window.setScene(view);
+        window.show();
     }
 
 }
