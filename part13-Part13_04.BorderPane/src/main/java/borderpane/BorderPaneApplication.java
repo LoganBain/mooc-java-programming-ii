ef1cc1989ffc29aba7a37b0580dd846e6c4505ea
LoganBain <loganbain23@gmail.com>
2022-09-29 20:07:27 +0100

    Completed Part 13 Exercise 4

--- part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
+++ part13-Part13_04.BorderPane/src/main/java/borderpane/BorderPaneApplication.java
@@ -1,11 +1,27 @@
 package borderpane;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
-public class BorderPaneApplication {
+public class BorderPaneApplication extends Application {
 
-
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    @Override
+    public void start(Stage window) {
+        BorderPane layout = new BorderPane();
+        layout.setTop(new Label("NORTH"));
+        layout.setRight(new Label("EAST"));
+        layout.setBottom(new Label("SOUTH"));
+        
+        Scene scene = new Scene(layout);
+        
+        window.setScene(scene);
+        window.show();
     }
 
+    public static void main(String[] args) {
+        launch(BorderPaneApplication.class);
+    }
 }
