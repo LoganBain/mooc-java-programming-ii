1a5bb348f344b187ccf817d981158f5536210107
LoganBain <loganbain23@gmail.com>
2022-09-30 20:11:33 +0100

    Completed Part 13 Exercise 7

--- part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
+++ part13-Part13_07.TextStatisticsPart2/src/main/java/textstatistics/TextStatisticsApplication.java
@@ -1,11 +1,59 @@
 package textstatistics;
 
+import java.util.Arrays;
+import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.stage.Stage;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.layout.BorderPane;
 
-public class TextStatisticsApplication {
 
+
+public class TextStatisticsApplication extends Application {
+
+    @Override
+    public void start(Stage window) {
+        BorderPane layout = new BorderPane();
+        Label lettersLabel = new Label("Letters: 0");
+        Label wordsLabel = new Label("Words: 0");
+        Label longestLabel = new Label("The longest word is: ");
+        TextArea inputArea = new TextArea("");
+        
+        layout.setCenter(inputArea);
+        
+        HBox texts = new HBox();
+        texts.setSpacing(10);
+        texts.getChildren().addAll(lettersLabel, wordsLabel, longestLabel);   
+        layout.setBottom(texts);
+        
+        inputArea.textProperty().addListener((change, oldValue, newValue) -> {
+            int characters = newValue.length();
+            String[] parts = newValue.split(" ");
+            int words = parts.length;
+            String longest = Arrays.stream(parts)
+                    .sorted((s1, s2) -> s2.length() - s1.length())
+                    .findFirst()
+                    .get();
+            
+            lettersLabel.setText("Letters: " + characters);
+            wordsLabel.setText("Words: " + words);
+            longestLabel.setText("The longest word is: " + longest);
+        });
+        
+        Scene view = new Scene(layout);
+        
+        window.setScene(view);
+        window.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(TextStatisticsApplication.class);
     }
 
 }
