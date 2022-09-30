8dd1149d58b17c76c0825b74d29f1f468989f72d
LoganBain <loganbain23@gmail.com>
2022-09-30 21:13:50 +0100

    Completed Part 13 Exercise 8

--- part13-Part13_08.UserTitle/src/main/java/title/Main.java
+++ part13-Part13_08.UserTitle/src/main/java/title/Main.java
@@ -1,11 +1,17 @@
 package title;
 
+import javafx.application.Application;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Give a title:");
+        String title = scanner.nextLine();
+        
+        Application.launch(UserTitle.class, "--title=" + title);
     }
 
 }
--- part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
+++ part13-Part13_08.UserTitle/src/main/java/title/UserTitle.java
@@ -1,7 +1,19 @@
 package title;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.application.Application.Parameters;
 
-public class UserTitle {
 
+public class UserTitle extends Application {
 
+    @Override
+    public void start(Stage window) throws Exception {
+        Parameters params = getParameters();
+        String title = params.getNamed().get("title");
+        
+        window.setTitle(title);
+        window.show();
+    }
+    
 }
