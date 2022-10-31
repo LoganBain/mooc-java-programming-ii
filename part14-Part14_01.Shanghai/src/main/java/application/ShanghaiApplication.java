178174c7290f7d8e66053b01709c6f881ff583b7
LoganBain <loganbain23@gmail.com>
2022-10-31 18:54:52 +0000

    Completed Part 14 Exercise 1

--- part14-Part14_01.Shanghai/src/main/java/application/ShanghaiApplication.java
+++ part14-Part14_01.Shanghai/src/main/java/application/ShanghaiApplication.java
@@ -1,9 +1,45 @@
 package application;
 
-public class ShanghaiApplication {
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
+import javafx.application.Application;
+
+public class ShanghaiApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(ShanghaiApplication.class);
     }
-
+    
+    public void start(Stage stage) {
+        NumberAxis xAxis = new NumberAxis(2006, 2018, 1);
+        NumberAxis yAxis = new NumberAxis(0, 100, 5);
+        
+        xAxis.setLabel("Year");
+        yAxis.setLabel("Ranking");
+        
+        XYChart.Series data = new XYChart.Series();
+        data.setName("");
+        data.getData().add(new XYChart.Data(2007, 73));
+        data.getData().add(new XYChart.Data(2008, 68));
+        data.getData().add(new XYChart.Data(2009, 72));
+        data.getData().add(new XYChart.Data(2010, 72));
+        data.getData().add(new XYChart.Data(2011, 74));
+        data.getData().add(new XYChart.Data(2012, 73));
+        data.getData().add(new XYChart.Data(2013, 76));
+        data.getData().add(new XYChart.Data(2014, 73));
+        data.getData().add(new XYChart.Data(2015, 67));
+        data.getData().add(new XYChart.Data(2016, 56));
+        data.getData().add(new XYChart.Data(2017, 56));
+        
+        LineChart chart = new LineChart(xAxis, yAxis);
+        chart.setTitle("University of Helsinki, Shanghai ranking");
+        chart.getData().add(data);
+        
+        Scene scene = new Scene(chart, 640, 480);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
