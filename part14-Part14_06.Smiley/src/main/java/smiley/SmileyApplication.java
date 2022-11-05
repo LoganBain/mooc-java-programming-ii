7eb236242586a79eb866391aa63fc3d0c5f339f3
LoganBain <loganbain23@gmail.com>
2022-11-05 19:57:57 +0000

    Completed Part 14 Exercise 6

--- part14-Part14_06.Smiley/src/main/java/smiley/SmileyApplication.java
+++ part14-Part14_06.Smiley/src/main/java/smiley/SmileyApplication.java
@@ -1,11 +1,35 @@
 package smiley;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
-public class SmileyApplication {
-
+public class SmileyApplication extends Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        launch(SmileyApplication.class);
+    }
+
+    public void start(Stage stage) {
+        BorderPane layout = new BorderPane();
+        Canvas canvas = new Canvas(300, 300);
+        GraphicsContext drawer = canvas.getGraphicsContext2D();
+
+        layout.setCenter(canvas);
+
+        drawer.setFill(Color.BLACK);
+        drawer.fillRect(70, 60, 25, 25);
+        drawer.fillRect(200, 60, 25, 25);
+        drawer.fillRect(75, 200, 150, 25); 
+        drawer.fillRect(75, 175, 25, 25); 
+        drawer.fillRect(200, 175, 25, 25);
+
+        stage.setScene(new Scene(layout));
+        stage.show();
     }
 
 }
