38ed72e52eac8d2840801c7f3ee18be0d59f25a4
LoganBain <loganbain23@gmail.com>
2022-11-10 11:18:32 +0000

    Initial Commit

--- part14-Part14_09.Asteroids/src/main/java/asteroids/AsteroidsApplication.java
+++ part14-Part14_09.Asteroids/src/main/java/asteroids/AsteroidsApplication.java
@@ -1,14 +1,71 @@
 package asteroids;
 
-public class AsteroidsApplication {
+import java.util.HashMap;
+import java.util.Map;
+import javafx.animation.AnimationTimer;
+import javafx.application.Application;
+import javafx.geometry.Point2D;
+import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Polygon;
+import javafx.stage.Stage;
+
+public class AsteroidsApplication extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Pane pane = new Pane();
+        pane.setPrefSize(600, 400);
+
+        Polygon ship = new Polygon(-5, -5, 10, 0, -5, 5);
+        ship.setTranslateX(300);
+        ship.setTranslateY(200);
+        ship.setRotate(30);
+
+        pane.getChildren().add(ship);
+
+        Scene scene = new Scene(pane);
+
+        Map<KeyCode, Boolean> pressedKeys = new HashMap<>();
+
+        scene.setOnKeyPressed(event -> {
+            pressedKeys.put(event.getCode(), Boolean.TRUE);
+        });
+
+        scene.setOnKeyReleased(event -> {
+            pressedKeys.put(event.getCode(), Boolean.FALSE);
+        });
+        
+        //Point2D movement = new Point2D(1, 0);
+
+        new AnimationTimer() {
+
+            @Override
+            public void handle(long now) {
+                if (pressedKeys.getOrDefault(KeyCode.LEFT, false)) {
+                    ship.setRotate(ship.getRotate() - 5);
+                }
+
+                if (pressedKeys.getOrDefault(KeyCode.RIGHT, false)) {
+                    ship.setRotate(ship.getRotate() + 5);
+                }
+                
+                //ship.setTranslateX(ship.getTranslateX() + movement.getX());
+            }
+        }.start();
+
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        launch(args);
     }
 
     public static int partsCompleted() {
         // State how many parts you have completed using the return value of this method
-        return 0;
+        return 1;
     }
 
 }
